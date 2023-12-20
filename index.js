956c94cb4c47932e466be943ce14e3bd99a52510
Eric Moss <1670755+dohnutt@users.noreply.github.com>
2023-12-19 23:55:01 -0500

    Not quite working yet but gettin' there
    Working thru promises :sweat_smile:

--- index.js
+++ index.js
@@ -3,30 +3,51 @@
  * Secondarily, sync into Letterboxd
  */
 
-const cheerio = require('cheerio');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 const axios = require('axios');
 
 // downloading the target web page
 async function scrapeIt() {
-	var items = [];
+	let document = {};
+	let items = [];
+	let prevFirstItem = null;
 
-	const axiosResponse = await axios.request({
-		method: 'GET',
-		url: 'https://www.google.com/collections/s/list/qJMxPOnLzoE4SU1ChkOhP3ZBC0hQdw/U8F-lG_6BmM?pageNumber=1',
-		headers: {
-			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+	for (let i = 0; i <= 5; i++) {
+		const axiosResponse = await axios.request({
+			method: 'GET',
+			url: 'https://www.google.com/collections/s/list/qJMxPOnLzoE4SU1ChkOhP3ZBC0hQdw/U8F-lG_6BmM?pageNumber=' + (i + 1),
+			headers: {
+				'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+			}
+		});
+
+		document = new JSDOM(axiosResponse.data).window.document;
+		items.push([]);
+
+		// Find and collect items
+		document.querySelectorAll('[data-hveid] a[aria-label]').forEach((el) => {
+			if (items[i][0] === prevFirstItem) {
+				return;
+			}
+
+			items[i].push(el.getAttribute('aria-label'));
+		});
+
+		if (items[i][0] === prevFirstItem) {
+			return;
 		}
-	});
 
-	const $ = cheerio.load(axiosResponse.data);
-	const pagesEl = $('[data-clamped-page-number]');
-	const els = $('[data-hveid]').find('a[aria-label]');
+		//console.log(items[i]);
 
-	els.each((i, el) => {
-		items.push( $(el).attr('aria-label') );
-	});
+		// Set first item for next iteration
+		prevFirstItem = items[i][0];
+		console.log((i + 1) + ': ' + prevFirstItem);
+	}
 
-	console.log(items);
+	//console.log(items);
+
+	return await items;
 }
 
 
